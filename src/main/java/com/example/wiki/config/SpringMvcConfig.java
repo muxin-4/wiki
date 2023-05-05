5432c16ea31c877996a7831c0d1315ddc3a23dbd
muxin <wgy952046097@gmail.com>
2023-05-05 10:28:58 +0800

    feat: 拦截器 修改excludePathPatterns

--- src/main/java/com/example/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/example/wiki/config/SpringMvcConfig.java
@@ -1,9 +1,5 @@
 package com.example.wiki.config;
 
-//import com.example.wiki.interceptor.ActionInterceptor;
-
-//import com.example.wiki.interceptor.LoginInterceptor;
-
 import com.example.wiki.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,33 +10,15 @@ import javax.annotation.Resource;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-//    @Resource
-//    LoginInterceptor loginInterceptor;
-
-    //    @Resource
-//    ActionInterceptor actionInterceptor;
     @Resource
     LogInterceptor logInterceptor;
 
     public void addInterceptors(InterceptorRegistry registry) {
+        System.out.println("进入addInterceptors");
         registry.addInterceptor(logInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns(
-                        "/test/**",
-                        "/redis/**",
-                        "/user/login",
-                        "/category/all",
-                        "/ebook/list",
-                        "/doc/all/**",
-                        "/doc/vote/**",
-                        "/doc/find-content/**",
-                        "/ebook-snapshot/**"
+                        "/login"
                 );
-
-//        registry.addInterceptor(actionInterceptor)
-//                .addPathPatterns(
-//                        "/*/save",
-//                        "/*/delete/**",
-//                        "/*/reset-password");
     }
 }
